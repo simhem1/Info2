4805feef3881dba1e49f2ea5a5558abb579587d8
Simon Hemmerich <simon.hemmerich@rub.de>
2016-04-20 21:12:48 +0200

    Second commit to Blatt1 and first to Blatt2

---
+++ Blatt2/src/Hello.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: 20.04.2016
+ *      Author: simon
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
+

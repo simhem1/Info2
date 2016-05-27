28592cc1fe86d3f6d2a5ac851aae539a2efb1ac1
Simon Hemmerich <simon.hemmerich@rub.de>
2016-05-27 23:21:14 +0200

    restoring files

---
+++ u3/a2/insertion/insertion_string.hpp
@@ -0,0 +1,17 @@
+/*
+ * insertion_string.hpp
+ *
+ *  Created on: 26.05.2016
+ *      Author: simon
+ */
+
+#ifndef U3_A2_INSERTION_INSERTION_STRING_HPP_
+#define U3_A2_INSERTION_INSERTION_STRING_HPP_
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> insertion_sort(std::vector<std::string> strings, char c);
+int compare_by_frequency(std::string a, std::string b, char c);
+
+#endif /* U3_A2_INSERTION_INSERTION_STRING_HPP_ */

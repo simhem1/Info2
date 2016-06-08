1f922439a86619344f5b2780c0258ee80864436f
Simon Hemmerich <simon.hemmerich@rub.de>
2016-06-08 21:04:50 +0200

    added u4_a1_1

---
+++ u4/reverse_quick_sort.cpp
@@ -0,0 +1,10 @@
+/*
+ * reverse_quicksort.cpp
+ *
+ *  Created on: 07.06.2016
+ *      Author: simon
+ */
+
+
+
+
---
+++ u4/reverse_quick_sort.hpp
@@ -0,0 +1,15 @@
+/*
+ * reverse_quick_sort.hpp
+ *
+ *  Created on: 07.06.2016
+ *      Author: simon
+ */
+
+#ifndef U4_A1_REVERSE_QUICK_SORT_HPP_
+#define U4_A1_REVERSE_QUICK_SORT_HPP_
+
+
+
+
+
+#endif /* U4_A1_REVERSE_QUICK_SORT_HPP_ */

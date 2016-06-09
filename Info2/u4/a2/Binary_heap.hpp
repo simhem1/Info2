343babd51a747f947cf0115114cfee40fe07d43c
Simon Hemmerich <simon.hemmerich@rub.de>
2016-06-09 19:32:38 +0200

    Copied code from template to Binary_heap.hpp

--- Info2/u4/a2/Binary_heap.hpp
+++ Info2/u4/a2/Binary_heap.hpp
@@ -9,6 +9,24 @@
 #define U4_A2_BINARY_HEAP_HPP_
 
 
+class Binary_heap
+ {
+ private:
+  std::vector<Apple> content;
+//  int N;
+
+ public:
+  Binary_heap();
+  bool is_empty();
+  void insert(Apple a);
+  Apple del_max();
+
+ private:
+  void swim(int k);
+  void sink(int k);
+  void exch(int i, int j);
+ };
+
 
 
 

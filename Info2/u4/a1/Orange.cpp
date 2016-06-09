15e53386111d72547b5a5ab7a0db08a99c59f672
Simon Hemmerich <simon.hemmerich@rub.de>
2016-06-09 18:19:36 +0200

    improved readability of conditional operator statement again

--- Info2/u4/a1/Orange.cpp
+++ Info2/u4/a1/Orange.cpp
@@ -7,7 +7,7 @@
 
 #include "Orange.hpp"
 
-Orange::Orange(int sweetness, int juicyness){
+Orange::Orange(int sweetness,  int juicyness){
 	this->sweetness=sweetness;
 	this->juicyness=juicyness;
 }
@@ -19,15 +19,22 @@ Orange::~Orange(){
 void Orange::setSweetness(const int sweetness){
 	this->sweetness=sweetness;
 }
+
 void Orange::setJuicyness(const int juicyness){
 	this->juicyness=juicyness;
 }
+
 const int Orange::getSweetness(void){
 	return sweetness;
 }
+
 const int Orange::getJuicyness(void){
 	return juicyness;
 }
+
 bool Orange::operator<(Orange other){
-	return ((((this->juicyness<(other.getJuicyness())) && (this->sweetness<(other.getSweetness()))) || ((this->sweetness==(other.getSweetness())) && ((this->juicyness<(other.getJuicyness()))))? true : false));
+	return
+		( (this->juicyness<( other.getJuicyness() ) ) && (this->sweetness<( other.getSweetness() ) ) ) ? true
+		: ( (this->sweetness==( other.getSweetness() ) ) && (this->juicyness<(other.getJuicyness() ) ) ) ? true
+		: false;
 }
